c38c671ce5a46162cdc2cd078b09eec6bec6081f
Matt Drees <matt.drees@cru.org>
2014-05-29 14:19:53 -0600

    add other webservices
    (also simplify service factory; the 2.3 version of the maven plugin makes this easier)

--- src/test/java/org/ccci/obiee/client/init/ServiceFactoryTest.java
+++ src/test/java/org/ccci/obiee/client/init/ServiceFactoryTest.java
@@ -3,8 +3,13 @@ package org.ccci.obiee.client.init;
 import com.infosolve.openmdm.webservices.provider.impl.DataManagementWSImpl;
 import com.infosolve.openmdm.webservices.provider.impl.DataManagementWSImplService;
 import com.infosolve.openmdm.webservices.provider.impl.RealTimeObjectActionDTO;
+import com.infosolvetech.rtmatch.pdi4.RuntimeMatchWS;
+import com.infosolvetech.rtmatch.pdi4.RuntimeMatchWSService;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import plugin.com.infosolvetech.ap.di.pdi4.AddressWS;
+import plugin.com.infosolvetech.ap.di.pdi4.AddressWSService;
+import plugin.org.pentaho.di.drools.jboss.pdi4.DroolsPluginWSService;
 
 public class ServiceFactoryTest
 {
@@ -20,13 +25,25 @@ public class ServiceFactoryTest
     @Test
     public void testDataManagementWSImplServiceCreation()
     {
-        DataManagementWSImpl port =
-            factory.buildService(DataManagementWSImplService.class).getDataManagementWSImplPort();
-
-
-        RealTimeObjectActionDTO object = port.findObject("234");
-
-        System.out.println(object);
-
+        factory.buildService(DataManagementWSImplService.class).getDataManagementWSImplPort();
     }
+
+    @Test
+    public void testRuntimeMatchWSServiceCreation()
+    {
+        factory.buildService(RuntimeMatchWSService.class).getRuntimeMatchWSPort();
+    }
+
+    @Test
+    public void testAddressWSServiceCreation()
+    {
+        factory.buildService(AddressWSService.class).getAddressWSPort();
+    }
+
+    @Test
+    public void testDroolsPluginWSServiceCreation()
+    {
+        factory.buildService(DroolsPluginWSService.class).getDroolsPluginWSPort();
+    }
+
 }
